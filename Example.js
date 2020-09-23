fd6cfdedad2e3d2c6c2f4b599c72c9976da2bb88
Alex-Shatskiy <alexlev706@gmail.com>
2020-09-23 12:25:41 +1200

    Sprint 4 core blog

---
+++ Example.js
@@ -0,0 +1,18 @@
+var arr = ["hello", 21, ["games", "outside"]]
+
+
+var house = {
+    "cold": true,
+    "bathroom": 1,
+    "bedrooms": 2,
+    "occupant": 4
+}
+
+
+// Function
+function add(num1, num2){
+    return num1 + num2;
+}
+
+//calling the function
+add(2,3) // 2 + 3 = 5
